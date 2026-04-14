2526546df5eb9c859e8aa452e9bb6cdd7c7b29c9
nathbiswa <biswanroy29@gmail.com>
2026-04-14 11:34:02 +0600

    hero card section done

--- src/Sections/HomeSection/HeroSection.jsx
+++ src/Sections/HomeSection/HeroSection.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const HeroSection = () => {
-    return (
-        <div>
-            Hero section
-        </div>
-    );
+  return (
+    <div className="bg-[#E9E9E9]">
+      <div className="  text-center py-10 space-y-3">
+        <h2 className="text-xl md:text-3xl font-bold">Friends to keep close in your life</h2>
+        <p className="text-gray-400">
+          Your personal shelf of meaningful connections. Browse, tend, and
+          nurture the <br /> relationships that matter most.
+        </p>
+        <button className="btn bg-[#244D3F] text-white">+ Add a Friend</button>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
--- src/Sections/HomeSection/HomeCart.jsx
+++ src/Sections/HomeSection/HomeCart.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const HomeCart = () => {
-    return (
-        <div>
-            Cart section
+  return (
+    <div className=" bg-[#E9E9E9] ">
+      <div className=" w-8/12 mx-auto text-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 py-8">
+        <div className="shadow-sm bg-white p-4 rounded-sm">
+          <span className="text-2xl font-bold">10</span>
+          <p className="text-gray-400">Total Friends</p>
         </div>
-    );
+        <div className="shadow-sm bg-white p-4 rounded-sm">
+          <span className="text-2xl font-bold">3</span>
+          <p className="text-gray-400">On Track</p>
+        </div>
+        <div className="shadow-sm bg-white p-4 rounded-sm">
+          <span className="text-2xl font-bold">6</span>
+          <p className="text-gray-400">Need Attention</p>
+        </div>
+        <div className="shadow-sm bg-white p-4 rounded-sm">
+          <span className="text-2xl font-bold">12</span>
+          <p className="text-gray-400">Interactions This Month</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeCart;
